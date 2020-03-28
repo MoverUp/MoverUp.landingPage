4554a82c10837559d935057c332117b20703a6f6
Arnaud Magnan <arnauze@gmail.com>
2020-03-28 17:49:17 +0100

    Updates

---
+++ precache-manifest.b0ae97747e5541b739107f1edfc9d6ad.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dba2706c3deb934227ddc18348a3668",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8614f541690720871433",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8614f541690720871433",
+    "url": "/static/js/2.58a0191d.chunk.js"
+  },
+  {
+    "revision": "404cdd2fbf60cad4a5d66ae1450a048a",
+    "url": "/static/js/2.58a0191d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "828d0bd6f582bce3c4da",
+    "url": "/static/js/main.212868ba.chunk.js"
+  },
+  {
+    "revision": "b82969bff53bd896edc9",
+    "url": "/static/js/runtime-main.5be55ab0.js"
+  },
+  {
+    "revision": "7100889bafa89cec1a0361077ce32902",
+    "url": "/static/media/add_slot.7100889b.png"
+  },
+  {
+    "revision": "b3bc727f27f8d89ede147aa8b282f6ae",
+    "url": "/static/media/android_logo.b3bc727f.png"
+  },
+  {
+    "revision": "7b9260538bcd4720b0d97e98f75a33e9",
+    "url": "/static/media/apple_logo.7b926053.png"
+  },
+  {
+    "revision": "14d2066a8fb2193463163b67af93852a",
+    "url": "/static/media/business.14d2066a.png"
+  },
+  {
+    "revision": "2efdcb3dd9b6be0871b03cde957d706e",
+    "url": "/static/media/choose_workers1.2efdcb3d.png"
+  },
+  {
+    "revision": "02561e245580ca21a5182971f23f6add",
+    "url": "/static/media/choose_workers2.02561e24.png"
+  },
+  {
+    "revision": "0519171c7e85949717ea011c2a725e6b",
+    "url": "/static/media/confirm_job.0519171c.png"
+  },
+  {
+    "revision": "94c668bf2fac6855c2a9aa49b38eb2dd",
+    "url": "/static/media/create_job1.94c668bf.png"
+  },
+  {
+    "revision": "6b2f9399b1bfb32a250a33e256a1e616",
+    "url": "/static/media/create_job2.6b2f9399.png"
+  },
+  {
+    "revision": "799e3c57495c9c2fa1b62272ca0d07f5",
+    "url": "/static/media/empty_calendar.799e3c57.png"
+  },
+  {
+    "revision": "27a063173e2554cc2af34c67d6c28cef",
+    "url": "/static/media/list_with_requests.27a06317.png"
+  },
+  {
+    "revision": "d9c391258b8a73afec5bc7f4f89a3f8e",
+    "url": "/static/media/movers.d9c39125.png"
+  },
+  {
+    "revision": "f130f48ae0b089ba6b538a422cc5f4ab",
+    "url": "/static/media/rate_helpers.f130f48a.png"
+  }
+]);
